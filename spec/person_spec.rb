0fd74fdee679da5088b4a3f63fb1cdc0695f87d1
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 12:02:06 +0100

    Class test passes

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,5 +2,11 @@ require "rspec"
 require_relative "../app/models/person"
 
 describe Person do
-  it "has the class Person"
+  it "has the class Person" do
+    person = Person.new("Tom", 26, "Male")
+    expect(person).to be_a(Integer)
+  end
+  it "has a String for a name" do
+
+  end
 end
