453f3464924960f2b2ee35c519aec6c06ab1481a
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 14:53:16 +0100

    pet_spec tests pass

--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -3,12 +3,12 @@ require_relative "../app/models/pet"
 
 describe Pet do
   it "checks the name is not empty" do
-    pet = Pet.new("", "Dog")
+    pet = Pet.new("Clive", "Dog")
     expect(pet.name.to_s).not_to be_empty
   end
 
   it "checks the type is 'Dog'" do
-    pet = Pet.new("Sean", "Sheep")
+    pet = Pet.new("Sean", "Dog")
     expect(pet.type).to eq("Dog")
   end
 end
