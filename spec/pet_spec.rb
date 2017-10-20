21ff035ad2f150d87371edb3885617091bb46a4a
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 16:56:11 +0100

    Add before :each method to pet_spec

--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -1,23 +1,22 @@
 require "rspec"
 require_relative "../app/models/pet"
 
-# First set, BAD PRACTICE
 describe Pet do
+
+  before :each do
+      @pet = Pet.new("Clive", "Dog")
+  end
+
   it "checks the name is not empty" do
-    pet = Pet.new("Clive", "Dog")
-    expect(pet.name.to_s).not_to be_empty
+    expect(@pet.name.to_s).not_to be_empty
   end
 
   it "checks the name contains a vowel" do
-    pet = Pet.new("Clive", "Dog")
-    expect(pet.name.to_s).to include("a").or(include("e")).or(include("i")).or(include("o")).or(include("u"))
+    expect(@pet.name.to_s).to include("a").or(include("e")).or(include("i")).or(include("o")).or(include("u"))
   end
 
   it "checks the type is 'Dog'" do
-    pet = Pet.new("Sean", "Dog")
-    expect(pet.type.to_s).to eq("Dog")
+    expect(@pet.type.to_s).to eq("Dog")
   end
 
 end
-
-# Second set, GOOD PRACTICE
