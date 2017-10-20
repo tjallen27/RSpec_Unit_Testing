4d949aaa4d8b906ddfdddb18b0b82522dc6c3a4a
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 15:45:43 +0100

    Test to check name includes a vowel

--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -7,8 +7,14 @@ describe Pet do
     expect(pet.name.to_s).not_to be_empty
   end
 
+  it "checks the name contains a vowel" do
+    pet = Pet.new("Clive", "Dog")
+    expect(pet.name.to_s).to include("a").or(include("e")).or(include("i")).or(include("o")).or(include("u"))
+  end
+
   it "checks the type is 'Dog'" do
     pet = Pet.new("Sean", "Dog")
-    expect(pet.type).to eq("Dog")
+    expect(pet.type.to_s).to eq("Dog")
   end
+
 end
