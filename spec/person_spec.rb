155ba70ad85430fc884692b18d7258fa760b724a
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 12:10:06 +0100

    Second test added: Passes on String

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,9 +4,10 @@ require_relative "../app/models/person"
 describe Person do
   it "has the class Person" do
     person = Person.new("Tom", 26, "Male")
-    expect(person).to be_a(Integer)
+    expect(person.class).to eq(Person)
   end
   it "has a String for a name" do
-
+    person = Person.new("Tom", 26, "Male")
+    expect(person.name).to be_a(String)
   end
 end
