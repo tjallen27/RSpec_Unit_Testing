b970489f2fc25b536ae4e1bddac76a797039e29f
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-20 11:37:44 +0100

    Build out person model and create set_age and init methods

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,19 @@
 class Person < ApplicationRecord
+  attr_accessor :name
+  attr_reader :age
+  attr_reader :gender
+
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def set_age(new_age)
+    if new_age < 1
+      @age = 0
+    else
+      @age = new_age
+    end
+  end
 end
