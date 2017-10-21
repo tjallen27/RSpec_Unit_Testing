40937e9683c0b1f421322d7d54621052731afca7
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-21 12:02:05 +0100

    Use Subject to refactor pet_spec

--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -1,22 +1,23 @@
 require "rspec"
 require_relative "../app/models/pet"
 
-describe Pet do
+RSpec.describe Pet, :name, :type => :model do
+  subject { described_class.new("Clive", "Dog") }
 
-  before :each do
-      @pet = Pet.new("Clive", "Dog")
-  end
+  # before :each do
+  #     @pet = Pet.new("Clive", "Dog")
+  # end
 
   it "checks the name is not empty" do
-    expect(@pet.name.to_s).not_to be_empty
+    expect(subject.name).not_to be_empty
   end
 
   it "checks the name contains a vowel" do
-    expect(@pet.name.to_s).to include("a").or(include("e")).or(include("i")).or(include("o")).or(include("u"))
+    expect(subject.name.to_s).to include("a").or(include("e")).or(include("i")).or(include("o")).or(include("u"))
   end
 
   it "checks the type is 'Dog'" do
-    expect(@pet.type.to_s).to eq("Dog")
+    expect(subject.type.to_s).to eq("Dog")
   end
 
 end
