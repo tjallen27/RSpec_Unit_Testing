c63a71e0b213112419f47837305cb9d93572e3de
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-23 16:22:17 +0100

    Added link on static home

---
+++ test/controllers/static_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_about_url
+    assert_response :success
+  end
+
+end
